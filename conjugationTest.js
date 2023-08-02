df04c13573fca5505c82ab0f67fa8a99f4b4c99a
Lucas <lucasegidio1@gmail.com>
2023-08-03 01:20:24 +0200

    ADD: dutch support

--- conjugationTest.js
+++ conjugationTest.js
@@ -1,12 +1,13 @@
-var json
-fetch('./data.json').then(response => {
-   return  response.json();
-}).then(jsondata => json = (jsondata)).then(()=>updateWord())
+const verb_data = {
+    "french": null,
+    "dutch": null
+}
 
+var currLanguage = "french"
 var currVerb;
 var currConj;
 var expAns = ""
-const checkboxes = Array.from(document.querySelectorAll("input[type='checkbox']"))
+var checkboxes = Array.from(document.querySelectorAll("input[type='checkbox']"))
 const verbInfo = document.getElementById("verbname")
 const tenseInfo = document.getElementById("verbtense")
 const inputText = document.getElementById("verbConj")
@@ -15,55 +16,83 @@ const feedback = document.getElementById("feedback")
 
 
 
-function getRandomVerb(){
-    k = Object.keys(json)
-    var i = Math.floor(Math.random()*k.length)   
+function getCheckboxHtml(i, tense){
+    return `<div class="tense">
+        <label id="labelt${i}" for="t${i}">${tense}</label>
+        <input class="form-check-input" type="checkbox" ${(i==0 )? 'checked="True"' : ""} name="${tense}" id="t${i}"><br>
+    </div>`
+}
+
+
+
+for (const language in verb_data) {
+    if (currLanguage == language) {
+        fetch(`./${language}.json`).then(response => {
+            return response.json();
+        }).then(jsondata => verb_data[language] = (jsondata)).then(() => setLanguage(currLanguage)).then(x => updateWord())
+    }
+    else {
+        fetch(`./${language}.json`).then(response => {
+            return response.json();
+        }).then(jsondata => verb_data[language] = (jsondata))
+    }
+}
+
+
+function setLanguage(language) {
+    currLanguage = language
+    const random_verb = getRandomVerb(language)
+    var idx = 0
+    var checkboxes_html = []
+    for (const tense in verb_data[language][random_verb]) {
+        checkboxes_html.push(getCheckboxHtml(idx, tense))
+        idx += 1
+    }
+    document.querySelector(".card").innerHTML = checkboxes_html.join('\n')
+    checkboxes = Array.from(document.querySelectorAll("input[type='checkbox']"))
+
+}
+
+function getRandomVerb(language) {
+    k = Object.keys(verb_data[language])
+    var i = Math.floor(Math.random() * k.length)
     return k[i]
 }
 
-function getConjugation(v){
-    const conjs = json[v]
+function getConjugation(language, v) {
+    const conjs = verb_data[language][v]
     const k = Object.keys(conjs)
-    const time = k[Math.floor(Math.random()*k.length)]
+    const time = k[Math.floor(Math.random() * k.length)]
     return [time, conjs[time]]
 }
 
-function updateWord(){
-    if (checkboxes.some(x=>x.checked))
-    {
-        const v = getRandomVerb()
+function updateWord() {
+    if (checkboxes.some(x => x.checked)) {
+        const v = getRandomVerb(currLanguage)
         var conj
-        do {   
-            conj = getConjugation(v)
-            
-        } while (!checkboxes.some(x => x.checked && x.name==conj[0]));
+        do {
+            [time, conj] = getConjugation(currLanguage, v)
+        } while (!checkboxes.some(x => x.checked && x.name == time));
         feedback.innerHTML = "<div class='answer'></div>"
         verbInfo.innerHTML = "<u>" + v + "</u>"
-        tenseInfo.innerHTML = "<em>" + conj[0] +"</em>:"
+        tenseInfo.innerHTML = "<em>" + time + "</em>:"
         currVerb = v
         currConj = conj
-        var i = Math.floor(Math.random()*conj[1].length)
-        var aux = conj[1][i].split(/\'| /)
-        person = aux.shift()
-        if (person == "que")
-            person += " " + aux.shift()
-        if (person == "qu")
-            person += "'" + aux.shift()
-        if(aux.length==0){
-            aux.unshift(person)
-            person = ""
-        }
-        expAns = aux.join(" ")
-        inputLabel.innerHTML = person + ((person[person.length-1]=="j")?"'":"");
+        pronouns = Object.keys(conj)
+        var i = Math.floor(Math.random() * pronouns.length)
+        var person = pronouns[i]
+
+        expAns = conj[person]
+        inputLabel.innerHTML = person;
         inputText.value = ""
-        if (person == "-")
+        if (expAns == "-")
             updateWord()
     }
 }
 
-function check(){
-    if (expAns !=""){
-        var text = inputText.value.split(" ").filter(x=>x.length>0).join(" ")
+function check() {
+    if (expAns != "") {
+        var text = inputText.value.split(" ").filter(x => x.length > 0).join(" ")
         if (expAns == text)
             feedback.innerHTML = '<div class="correct answer">Correct!</div>'
         else
@@ -73,30 +102,13 @@ function check(){
 }
 
 
-// 
-//
-
-
-// var coll = document.getElementsByClassName("collapsible");
-// var i;
-
-// for (i = 0; i < coll.length; i++) {
-//   coll[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     var content = this.nextElementSibling;
-//     if (content.style.display === "block") {
-//       content.style.display = "none";
-//     } else {
-//       content.style.display = "block";
-//     }
-//   });
-// }
-
-document.querySelector("#verbConj").addEventListener("keyup",(event)=>{
-    if(event.keyCode==38)
+document.querySelector("#verbConj").addEventListener("keyup", (event) => {
+    if (event.keyCode == 38)
         updateWord()
 })
 
 $('#collapseTenses').on('shown.bs.collapse', function () {
     this.scrollIntoView();
-  });
+});
+
+
